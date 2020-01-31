2f357fd0617e436a2bed1a8fc7dadcfbde8687c2
Claudia Pierson <c172@umbc.edu>
2020-01-31 04:34:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(sentence)
 new_sentence = []
-  sentence.split.each do |backward_words|
+  sentence.split.collect |backward_words|
     new_sentence << backward_words.reverse
 end
 new_sentence.join(" ")
 end
-reverse_each_word("Hello there, and how are you?")
- end
