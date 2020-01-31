ebf39ce96f3a7ef72cf394688ffd3e781b67ba12
Claudia Pierson <c172@umbc.edu>
2020-01-31 04:34:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
 new_sentence = []
-  sentence.split.collect |backward_words|
+  sentence.collect |backward_words|
     new_sentence << backward_words.reverse
 end
 new_sentence.join(" ")
